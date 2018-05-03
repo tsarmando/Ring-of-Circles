60e09435257477123c191ce08ca2f3c074a1aad6
tsarmando <magicmonkey511@gmail.com>
2018-05-03 14:59:01 -0700

    animation works

--- circle.js
+++ circle.js
@@ -1,31 +1,60 @@
-//import "circles_intersect.js"
 window.onload = function (){
-	var canvas = document.getElementById("canvas"),
+
+	const canvas = document.getElementById("canvas"),
 		ctx = canvas.getContext("2d"),
 		width = canvas.width = window.innerWidth,
 		height = canvas.height = window.innerHeight;
-	var y_0= height *.5, x_0 = width*.5; //represents orgin
+		//represent orgin:
+		const y_0 = height *.5, x_0 = width*.5; 
 
-	var dist = (p1,p2) =>  Math.sqrt(Math.pow(p2[0] - p1[0],2) +  Math.pow( p2[1] - p1[1],2))
+	const dist = (p1,p2) =>  Math.sqrt(Math.pow(p2[0] - p1[0],2) +  Math.pow( p2[1] - p1[1],2))
 
-	function kissing(n,r){
-//	ctx.clearRect(0,0,width,height);
-	const delta = 2*Math.PI/n, r0 = dist([x_0+r*Math.sin(0),y_0+r*Math.cos(0)], [x_0+r*Math.sin(delta),y_0+r*Math.cos(delta)])/2
-        let theta = 0
-	for(let i = 0; i<n;i++){
-		theta += delta; 
-		ctx.beginPath();
-		ctx.arc(x_0+r* Math.sin(theta),y_0+r * Math.cos(theta),r0,0,Math.PI*2, false);
-		ctx.stroke();
+	const kissing = (n,r, alpha, color) => {//number of circles, radius they lie on
+		const delta = 2*Math.PI/n+alpha, 
+		//distance between two kissing with centers on same circle
+		r0 = dist([x_0+r*Math.sin(0),y_0+r*Math.cos(0)], [x_0+r*Math.sin(delta),y_0+r*Math.cos(delta)])/2 
+		let theta = 0
+		for(let i = 0; i<n;i++){
+			theta += delta; 
+			ctx.beginPath();
+			ctx.arc(x_0+r* Math.sin(theta),y_0+r * Math.cos(theta),r0,0,Math.PI*2, false);
+			ctx.strokeStyle = "green" 
+			ctx.stroke();
+		}
+		//return [r+r0,r0]
 	}
-	return r0
-	}
-//kissing(20,kissing(2,25)+27)
-kissing(2,40)
-//kissing(3,75)
 
-//kissing(4,100)
-//kissing(5,120)
-kissing(300,500)
+	class Ring {
+		constructor(numCirc, distOrgin, initAngle,color) {
+			this.n = numCirc
+			this.r = distOrgin 
+			this.theta = initAngle
+			this.color = color
+		}	
+		draw(newAng){
+			this.theta = newAng
+			kissing(this.n,this.r,newAng,"black")	
+		}
+	}
+
+	let animArr = []
+	let angle = 0;
+		for(let k = 15; k<30; k++){
+			animArr.push(new Ring(k,65,angle,"black"))
+		//	kissing(k,65,angle, "black")
+		//	console.log(k, newR,x)
+			for(j = 0; j< height; j+=50){ 
+				animArr.push(new Ring(k,100+j,angle,"black")) 
+			}
+		}
+	const go = () => { ticks = 0; setInterval(animate,300)}
+	const animate = () => {
+	//	const startTime = performance.now();
+		ticks++
+		ctx.clearRect(0,0,width,height)
+		animArr.forEach( el => el.draw(angle+ticks*Math.PI/360)) 
+	      //const duration = performance.now() - startTime;
+              //console.log(`someMethodIThinkMightBeSlow took ${duration}ms`,ticks);
+	}
+	go()
 }
-
--- circle_math.js
+++
@@ -1,3 +0,0 @@
-var circleIntersects = (pos1, pos2) => {
-	const r = Math.sqrt(Math.pow(2,(pos1[0]-pos2[0]))+Math.pow(2,(pos1[1]-pos2[1])))
-}
